e1b9c23c7cab66903fdc15deed2509444c62e6cd
Emilse Arias <emilsearias@gmail.com>
2025-07-23 11:43:33 -0300

    modifique el filter del array y agregue un parrafo cuando no hay productos que coincidan con la busqueda

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,13 +1,17 @@
 import { Container, Row, Form } from "react-bootstrap";
 import CardProducto from "./producto/CardProducto";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Inicio = ({ productos }) => {
-  const [terminoBusqueda, setTerminoBusqueda] = useState('')
+  const [terminoBusqueda, setTerminoBusqueda] = useState("");
 
-  const handleInputChange = (e)=>{
-    setTerminoBusqueda(e.target.value)
-  }
+  const handleInputChange = (e) => {
+    setTerminoBusqueda(e.target.value);
+  };
+
+  const productosFiltrados = productos.filter((producto) => 
+    producto.nombreProducto.toLowerCase().includes(terminoBusqueda.toLowerCase())
+  );
 
   return (
     <section className="mainSection">
@@ -22,13 +26,23 @@ const Inicio = ({ productos }) => {
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Buscar un producto</Form.Label>
-            <Form.Control type="text" placeholder="Ingresa el nombre del producto" onChange={handleInputChange} value={terminoBusqueda}/>
+            <Form.Control
+              type="text"
+              placeholder="Ingresa el nombre del producto"
+              onChange={handleInputChange}
+              value={terminoBusqueda}
+            />
           </Form.Group>
         </Form>
         <Row>
-          {productos.map((producto) => (
-            <CardProducto key={producto.id} producto={producto}></CardProducto>
-          ))}
+          {productosFiltrados.length > 0
+            ? productosFiltrados.map((producto) => (
+                <CardProducto
+                  key={producto.id}
+                  producto={producto}
+                ></CardProducto>
+              ))
+            : <p>No hay productos disponibles</p>}
         </Row>
       </Container>
     </section>
